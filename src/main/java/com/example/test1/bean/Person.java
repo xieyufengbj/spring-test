43e8d101d6483fa81797fcef47906e7ffc48b893
余丰 <xieyufeng@ichangtou.com>
2020-03-02 10:04:11 +0800

    test01

---
+++ src/main/java/com/example/test1/bean/Person.java
@@ -0,0 +1,16 @@
+package com.example.test1.bean;
+
+import lombok.Data;
+
+/**
+ * FileName: person
+ *
+ * @author: yufeng
+ * @date: 2020/3/2 08:39
+ * @description:
+ */
+@Data
+public class Person {
+    private String name;
+    private String age;
+}
